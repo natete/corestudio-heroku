7dce0e61c79601337df6b8c7e8a334db035ee159
natete <natete981@gmail.com>
2015-10-22 22:20:00 +0200

    Add logout

--- src/main/webapp/js/app/controllers/NavController.js
+++ src/main/webapp/js/app/controllers/NavController.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('corestudioApp.controllers')
-.controller('NavController', ['authService', '$scope', function(authService, $scope) {
+    .controller('NavController', ['authService', '$scope', function (authService, $scope) {
         $scope.user = authService.user;
 
-    if(authService.user) {
-        $scope.loggedIn = true;
-    } else {
-        $('#wrapper').css('padding-left', '0');
-    }
-}]);
+        if (authService.user) {
+            $scope.loggedIn = true;
+        } else {
+            $('#wrapper').css('padding-left', '0');
+        }
+
+        $scope.logout = function() {
+            authService.logout();
+        }
+
+    }]);
 
