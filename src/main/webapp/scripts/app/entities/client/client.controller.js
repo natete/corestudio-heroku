cd1365256c9d3e0e6bb12f48193282eab7cac7bf
natete <natete981@gmail.com>
2015-11-09 21:26:28 +0100

    Smart tables

--- src/main/webapp/scripts/app/entities/client/client.controller.js
+++ src/main/webapp/scripts/app/entities/client/client.controller.js
@@ -13,26 +13,14 @@
     function ClientController(Client) {
         var vm = this;
 
-        vm.uiGrid = {
-            paginationPageSizes: [10, 25, 50],
-            paginationPageSize: 10,
-            enableHorizontalScrollbar : 0,
-            enableVerticalScrollbar : 0,
-            enableColumnMenus : false,
-            enableRowHashing: true,
-            columnDefs: [
-                { name: 'photo', displayName: '' },
-                { name: 'name', displayName: 'Nombre' },
-                { name: 'classes', displayName: 'Clases pendientes' },
-            ]
-        };
+        vm.data = {};
 
         activate();
 
         ////////////////
 
         function activate() {
-            vm.uiGrid.data = Client.query();
+            vm.data = Client.query();
         }
     }
 })();
