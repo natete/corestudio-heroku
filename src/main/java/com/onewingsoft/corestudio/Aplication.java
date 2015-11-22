2b5d84ed38bdb3f9e28e99dd1836ccb38ba13430
natete <natete981@gmail.com>
2015-11-22 15:46:42 +0100

    Fix date problem

--- src/main/java/com/onewingsoft/corestudio/Aplication.java
+++ src/main/java/com/onewingsoft/corestudio/Aplication.java
@@ -1,10 +1,15 @@
 package com.onewingsoft.corestudio;
 
+import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.TimeZone;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -13,5 +18,10 @@ public class Aplication extends SpringBootServletInitializer {
 
 	public static void main(String args[]) {
 		SpringApplication.run(Aplication.class, args);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
 	}
 }
