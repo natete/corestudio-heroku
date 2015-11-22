24827e205d9154c776e6d98505df8b5d9e2726c7
natete <natete981@gmail.com>
2015-11-22 15:51:57 +0100

    Fix date problem

--- src/main/java/com/onewingsoft/corestudio/Aplication.java
+++ src/main/java/com/onewingsoft/corestudio/Aplication.java
@@ -1,6 +1,5 @@
 package com.onewingsoft.corestudio;
 
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
